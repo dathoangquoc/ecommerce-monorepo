1ccd73a4a358f04e52e0fe0dab64ee24d2e12253
dathoangquoc <dathgqc@gmail.com>
2025-10-15 15:28:11 +0700

    feat: login ui prototype

--- apps/web/src/app/auth/page.tsx
+++ apps/web/src/app/auth/page.tsx
@@ -1,3 +1,15 @@
 export default function Page() {
-    return <h1>Authenticating...</h1>
+    return (
+        <form action="">
+            <div>
+                <label htmlFor="email">Email</label>
+                <input type="text" id="email" name="email" placeholder="Email" />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type="text" id="password" name="password" placeholder="Password" />
+            </div>
+            <button type="submit">Log In</button>
+        </form>
+    )
 }
