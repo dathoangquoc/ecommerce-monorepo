5bc8574bb53b459a7ed4df0327da743f728c119e
dathoangquoc <dathgqc@gmail.com>
2025-10-17 10:39:31 +0700

    feat: add login ui

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,15 @@
 import './globals.css'
 
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSet,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
 export default async function Page() {
   let message = "API offline..."
 
@@ -11,19 +21,29 @@ export default async function Page() {
   }
 
   return (
-    <>
-    <h1>{message}</h1>
-    <form action="">
-          <div>
-              <label htmlFor="email">Email</label>
-              <input type="text" id="email" name="email" placeholder="Email" />
-          </div>
-          <div>
-              <label htmlFor="password">Password</label>
-              <input type="text" id="password" name="password" placeholder="Password" />
-          </div>
-          <button type="submit">Log In</button>
-        </form>
-    </>
+    <main className='mx-auto max-w-6xl p-5 border'>
+      <h1 className=''>{message}</h1>
+      <div className="w-full max-w-md">
+      <FieldSet>
+        <FieldGroup>
+          <Field>
+            <FieldLabel htmlFor="username">Username</FieldLabel>
+            <Input id="username" type="text" placeholder="Max Leiter" />
+            <FieldDescription>
+              Choose a unique username for your account.
+            </FieldDescription>
+          </Field>
+          <Field>
+            <FieldLabel htmlFor="password">Password</FieldLabel>
+            <FieldDescription>
+              Must be at least 8 characters long.
+            </FieldDescription>
+            <Input id="password" type="password" placeholder="********" />
+          </Field>
+        </FieldGroup>
+        <Button type="submit">Submit</Button>
+      </FieldSet>
+    </div>
+    </main>
   );
 }
